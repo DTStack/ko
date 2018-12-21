6dc46ff80610faa00cf736a92b8164186b73d96d
yangyi <yhxyangyi@163.com>
2018-12-21 10:12:13 +0800

    自定义-webpack

--- config/getUserConf.js
+++ config/getUserConf.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const fs = require('fs');
-const userConfFile = 'ko.config.json';
+const userConfFile = 'ko.config.js';
 const { getCurFilePath } = require('../util');
+const webpack=require('webpack');
 function getUserConf () {
     let curFilePath = getCurFilePath(userConfFile);
     if (fs.existsSync(curFilePath)) {
-        return require(curFilePath);
+        return require(curFilePath)({webpack});
     } else {
         return {
             proxy:[],
@@ -18,6 +19,7 @@ function getUserConf () {
 
 module.exports = () => {
     const userConf=getUserConf();
+    console.log(userConf,'http://172.16.8.170/webapp.html#/home');
     const {proxy={},server={},webpack={},move={}}=userConf;
      return{
         proxy,
