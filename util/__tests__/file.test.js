244f39bf6736d4ea3694658364e913ca5b335857
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-06-17 11:03:02 +0800

    test: add unit test, file service

--- util/__tests__/file.test.js
+++ util/__tests__/file.test.js
@@ -6,12 +6,15 @@ describe('file utils:', () => {
     expect(getFileRealPath).toBeInstanceOf(Function);
   });
 
-  // it ('getFileRealPath', () => {
+  it('getFileRealPath', () => {
 
-  //   const realPath = getFileRealPath('./a.js');
-  //   expect(realPath).toBe(
-  //     path.resolve(__dirname, 'a.js')
-  //   )
-  // })
+    const workspace = process.cwd();
+    const dirname = 'build';
+
+    const realPath = getFileRealPath(dirname);
+    expect(realPath).toBe(
+      path.resolve(workspace, dirname)
+    )
+  })
 });
 
--- util/__tests__/fileService.test.js
+++ util/__tests__/fileService.test.js
@@ -12,20 +12,22 @@ const fs = require('fs');
 const path = require('path');
 
 // global data filed
-const str = 'this is string test string...';
-const filename = 'test.txt';
-const completePath = path.resolve(__dirname, filename);
+const mockContent = 'this is string test string...';
+const mockFilename = 'test.txt';
+const completePath = path.resolve(__dirname, mockFilename);
 const errorFileNotPrepared = new Error('testing file is not prepared...');
+const mockDirname = 'mock';
+const completeDirPath = path.resolve(__dirname, mockDirname);
 
 describe('file service:', () => {
   it('create file sync:', () => {
-    createFileSync(completePath, str);
+    createFileSync(completePath, mockContent);
     const content = fs.readFileSync(completePath).toString();
-    expect(content).toBe(str);
+    expect(content).toBe(mockContent);
   });
 
   it('create write stream:', done => {
-    fs.writeFileSync(completePath, str);
+    fs.writeFileSync(completePath, mockContent);
     if (!fs.existsSync(completePath))
       throw errorFileNotPrepared;
     const writeStream = createWriteStream(completePath);
@@ -40,17 +42,68 @@ describe('file service:', () => {
   });
 
   it('read file sync:', () => {
-    fs.writeFileSync(completePath, str);
+    fs.writeFileSync(completePath, mockContent);
     if (!fs.existsSync(completePath))
       throw errorFileNotPrepared;
     const result = readFileSync(completePath);
     const content = fs.readFileSync(completePath).toString();
     expect(result).toBe(content);
+  });
+
+  it('exists sync:', () => {
+    const notExit = existsSync(completePath);
+    expect(notExit).toBe(false);
+    fs.writeFileSync(completePath, mockContent);
+    if (!fs.existsSync(completePath))
+      throw errorFileNotPrepared;
+    const exit = existsSync(completePath);
+    expect(exit).toBe(true);
+  });
+
+  it('mkdir', () => {
+    mkdir(completeDirPath);
+    const result = fs.existsSync(completeDirPath);
+    expect(result).toBe(true);
+    try {
+      mkdir(completeDirPath);
+    } catch(error) {
+      expect(error.message).toMatch(/EEXIST: file already exists/);
+    }
+  });
+
+  it('mkdir access control:', () => {
+    mkdir(completeDirPath);
+    fs.accessSync(completeDirPath, fs.constants.W_OK | fs.constants.R_OK);
+  });
+
+  it('getCurFilePath', () => {
+    const dirname = 'mock';
+    const result = getCurFilePath(dirname);
+    const workspace = process.cwd();
+    expect(result).toBe(
+      path.resolve(workspace, dirname)
+    );
+  });
+
+  it('get absolute path:', () => {
+    const relationPath = './build';
+    const absolutePath = '/Users';
+    const relationResult = isAbsolute(relationPath);
+    const absoluteResult = isAbsolute(absolutePath);
+    const workspace = process.cwd();
+    expect(relationResult).toBe(
+      path.resolve(workspace, relationPath)
+    );
+    expect(absoluteResult).toBe(absoluteResult);
   })
 
-  afterEach(() => {
+
+  afterEach (() => {
     if (fs.existsSync(completePath)) {
       fs.unlinkSync(completePath);
+    }
+    if (fs.existsSync(completeDirPath)) {
+      fs.rmdirSync(completeDirPath);
     }
   })
 })
