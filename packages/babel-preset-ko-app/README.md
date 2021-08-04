9a8d1dfd41b64b4aeb6110d891d9bc14a93ba6d0
xbrave <sunzy1993@hotmail.com>
2021-08-04 19:55:44 +0800

    feat: init babel-preset-ko-app

---
+++ packages/babel-preset-ko-app/README.md
@@ -0,0 +1,17 @@
+# babel-preset-ko-app
+
+This package includes the Babel preset used by [ko](https://github.com/DTStack/ko)
+
+## Usage Outside of ko
+
+please checkout how to [Using a Preset](https://babeljs.io/docs/en/presets#using-a-preset)
+
+```json
+{
+  "presets": ["ko-app"]
+}
+```
+
+## Output targets
+
+We assume that you know Babel Config Option [Output targets](https://babeljs.io/docs/en/options#output-targets).And it's great to use [`browserslist`](https://github.com/browserslist/browserslist) to manage babel output targets.
---
+++ packages/babel-preset-ko-app/index.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = function (api, opts = {}) {
+  let absoluteRuntimePath = false;
+  if (opts.useAbsoluteRuntime) {
+    absoluteRuntimePath = path.dirname(
+      require.resolve('@babel/runtime/package.json')
+    );
+  }
+  const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+  const isProd = env === 'production';
+  return {
+    assumptions: {
+      //use assignment rather than using Object.defineProperty
+      setPublicClassFields: true,
+    },
+    presets: [
+      [
+        require('@babel/preset-env').default,
+        {
+          useBuiltIns: 'entry',
+          corejs: 3,
+          // Exclude transforms that make all code slower
+          exclude: ['transform-typeof-symbol'],
+        },
+      ],
+      [
+        require('@babel/preset-react').default,
+        {
+          development: !isProd,
+        },
+      ],
+    ],
+    plugins: [
+      //support proposal decorators
+      //NOTE: https://babeljs.io/docs/en/babel-plugin-proposal-decorators#note-compatibility-with-babelplugin-proposal-class-properties
+      [
+        require('@babel/plugin-proposal-decorators').default,
+        { legacy: true },
+      ],
+      [
+        require('@babel/plugin-transform-runtime').default,
+        {
+          corejs: false,
+          version: require('@babel/runtime/package.json').version,
+          regenerator: true,
+          absoluteRuntime: absoluteRuntimePath,
+        },
+      ],
+    ],
+  };
+};
