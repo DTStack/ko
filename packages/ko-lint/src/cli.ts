264f4bb0fbfe2922941486efcb170fa97f3680a3
xbrave <sunzy1993@hotmail.com>
2021-08-10 16:37:00 +0800

    feat(ko eslint): ko lint init

--- packages/ko-lint/src/cli.ts
+++ packages/ko-lint/src/cli.ts
@@ -6,6 +6,11 @@ import { defaultPatterns } from './constants';
 import { getTargetFiles } from './utils';
 import { formatFilesWithPrettier } from './prettier';
 
+type Option = {
+  write?: boolean;
+  configPath?: string;
+}
+
 const pkg = require('../package.json');
 
 program.description('ko lint tools').version(pkg.version, '-v --version');
@@ -16,17 +21,19 @@ program
   .description('use prettier to format your codes')
   .option('-w, --write', 'Edit files in-place. (Beware!)')
   .option('-c, --config <configPath>', 'set prettier config path')
-  .action((patterns = defaultPatterns, opts) => {
+  .action((patterns: string = defaultPatterns, opts: Option) => {
     const { write, configPath } = opts;
     const targetFiles = getTargetFiles(patterns);
     formatFilesWithPrettier(targetFiles, !write, configPath);
   });
 
 program
-  .command('eslint')
+  .command('eslint [patterns]')
   .alias('es')
   .description('use eslint to format your codes')
-  .option('-c, --config <path>', 'set eslint config path');
-// .action(opts => {});
+  .option('-c, --config <path>', 'set eslint config path')
+  .action((patterns: string = defaultPatterns, ops: Option) => {
+    
+  });
 
 program.parse();
