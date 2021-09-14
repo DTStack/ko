076d02fdf4803ac11ccce403fed3b7eabd67299b
ShuangXu <shuangxu@FBBdeMacBook-Pro.local>
2021-09-14 19:27:03 +0800

    feat: update eslint

--- packages/ko-lints/src/eslint.ts
+++ packages/ko-lints/src/eslint.ts
@@ -1,6 +1,7 @@
 import { findRealPath } from "./utils";
 import { ESLint } from 'eslint'
 import { promises } from "fs";
+const ESLINT_FILETYPE = ['ts', 'js', 'jsx', 'tsx', 'json', 'JSON'];
 
 export async function formatFilesWithEslint(
   files: string[],
@@ -10,15 +11,24 @@ export async function formatFilesWithEslint(
     ? require(findRealPath(configPath))
     : require("ko-config/eslint")
   console.log('eslint process starting....')
-  const eslint = new ESLint({ fix: true, baseConfig: eslintConfig })
+  const eslint = new ESLint({ fix: !isCheck, overrideConfig: eslintConfig })
   const eslintFilesPromises = files.map(async (file) => {
     try {
-      const result = await eslint.lintFiles(file)
-      const { errorCount, output } = result[0]
-      if (isCheck) {
-        return !!errorCount
+      let result;
+      let resultText;
+      const type = file.split('.').pop() || ''
+      if (ESLINT_FILETYPE.includes(type)) {
+        result = await eslint.lintFiles(file)
+        await ESLint.outputFixes(result);
+        const formatter = await eslint.loadFormatter("stylish");
+        resultText = formatter.format(result);
+        resultText && console.log(resultText);
+        const { errorCount, output } = result[0]
+        if (isCheck) {
+          return !errorCount
+        }
+        return output
       }
-      return output && promises.writeFile(file, output, 'utf-8');
     } catch (ex) {
       throw ex
     }
