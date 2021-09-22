aaefcc488fe015e5cbcdfbc53cc278c13468c2f2
xuxiaoqi <zhaoge@dtstack.com>
2021-09-22 18:04:26 +0800

    feat: suppprt stylelint

--- packages/ko-config/index.js
+++ packages/ko-config/index.js
@@ -1,7 +1,8 @@
 const prettier = require('./prettier');
 const eslint = require('./eslint');
-
+const stylelint = require('./stylelint');
 module.exports = {
   prettier,
   eslint,
+  stylelint
 };
---
+++ packages/ko-lints/src/stylelint.ts
@@ -0,0 +1,37 @@
+import { findRealPath } from "./utils";
+import * as stylelint from 'stylelint';
+import { StylelintOptions } from './interfaces';
+
+export async function formatFilesWithStylelint(opts: StylelintOptions & { targetFiles: string[] } ) {
+    const { configPath, fix, targetFiles } = opts;
+    const config = configPath
+    ? require(findRealPath(configPath))
+    : require('ko-config/stylelint');
+    console.log('stylelint process starting...');
+    try {
+        let options: StylelintOptions = {};
+        if (config.hasOwnProperty('files')) {
+            options.files = targetFiles?.length ? targetFiles : (config.files || []);
+            delete config.files;
+        }
+        if (config.hasOwnProperty('code')) {
+            options.code = config.code || '';
+            delete config.code;
+        }
+        const result = await stylelint.lint({
+            fix,
+            config,
+            ...options
+        });
+        if (result.errored) {
+            const resultText = stylelint.formatters.string(result.results);
+            console.log('Not all matched style files are linted: ');
+            console.log(resultText);
+        } else {
+            console.log('All matched style files has been fixed successfully!');
+        }
+        return true;
+    } catch (ex) {
+        console.log('stylelint failed: ', ex)
+    }
+}
