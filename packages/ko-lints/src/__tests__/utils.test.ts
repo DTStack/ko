ae69c1ef713e46893b796bfbc57929e04f9d108b
ShuangXu <shuangxu@FBBdeMacBook-Pro.local>
2021-10-20 19:20:35 +0800

    feat: add test for ko-lints utils-findRealPath

---
+++ packages/ko-lints/src/__tests__/utils.test.ts
@@ -0,0 +1,19 @@
+import { findRealPath } from '../utils';
+import { join } from 'path';
+
+describe('utils test--findRealPath', () => {
+  it('find absolute path', () => {
+    const path1 = process.cwd() + "/packages/ko-config/eslint.js";
+    expect(findRealPath(path1)).toBe(path1);
+  })
+  it('find relative path', () => {
+    const path2 = 'packages/ko-config/eslint.js'
+    expect(findRealPath(path2)).toBe(join(process.cwd(), path2));
+  })
+  it('error path', () => {
+    const errorAbsolutePath = process.cwd() + "/packages/ko-config/eslint1.js";
+    expect(() => { findRealPath(errorAbsolutePath) }).toThrow();
+    const errorRelativePath = process.cwd() + "packages/ko-config/eslint1.js";
+    expect(() => { findRealPath(errorRelativePath) }).toThrow();
+  })
+})
