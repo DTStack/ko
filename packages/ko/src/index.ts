ee24953f13bf6829a653d8282df539b7c6423f97
xbrave <sunzy1993@hotmail.com>
2022-06-28 13:46:59 +0800

    feat: update package configs

---
+++ packages/ko/src/index.ts
@@ -0,0 +1,4 @@
+export { default as Service } from './core/service';
+export { default as Dev } from './actions/dev';
+export { default as Build } from './actions/build';
+export { default as LintFactory } from './actions/lints';
